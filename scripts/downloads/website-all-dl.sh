ee966cfc59f3cbcfe1581dd47fd1e0e1415b79e1
Elle Alannah Marks <140675814+ellemarks@users.noreply.github.com>
2026-07-22 15:33:24 +0800

    Create website-all-dl.sh

---
+++ scripts/downloads/website-all-dl.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+WMD=~/.local/share/gem/ruby/3.4.0/bin/wayback_machine_downloader
+ONLY="/\.(jpg|jpeg|png|gif|webp|svg|ico|mp4|mov|avi|wmv|mkv|webm|flv|m4v|mp3|wav|ogg)$/i"
+
+for url in \
+  "https://www.onenation.org.au/*" \
+  "https://assets.nationbuilder.com/onenation/*" \
+  "https://d3n8a8pro7vhmx.cloudfront.net/onenation/*" \
+  "https://www.senatorhanson.com.au/*"
+do
+  domain=$(echo "$url" | sed 's|https://||;s|/\*||')
+  $WMD "$url" \
+    --only "$ONLY" \
+    --concurrency 5 \
+    --directory "../../data/one-nation/website/onenation.org.au/${domain}/"
+done
+ 
+for subdomain in i0 i1 i2; do
+  $WMD "https://${subdomain}.wp.com/www.senatorhanson.com.au/*" \
+    --only "$ONLY" \
+    --concurrency 5 \
+    --directory "../../data/one-nation/website/onenation.org.au/${subdomain}.wp.com/"
+done
